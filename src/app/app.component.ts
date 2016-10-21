5046ad917a8a05f2ed3aca2b32873941cc8409d8
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2016-10-21 16:35:41 +0200

    Add common styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
-  styleUrls: [ './app.component.css' ],
+  styleUrls: [ './app.component.css', './common.style.css' ],
   template: `<div devices></div>`
   
 })
